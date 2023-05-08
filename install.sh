f2ee3c37c1061aa57a55ad3d25b5e859274a7f07
roocell <roocell@users.noreply.github.com>
2023-05-08 15:35:29 -0400

    install run-through #1

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 
 sudo apt update
 sudo apt full-upgrade
-sudo apt install xdotool unclutter
+sudo apt -y install xdotool unclutter
+# sudo bash -c "echo -e 'export DISPLAY=:0.0' >> /root/.bashrc"
+# sudo bash -c "echo -e 'export DISPLAY=:0.0' >> /home/pi/.bashrc"
+sudo bash -c "echo -e 'autologin-user=pi\nautologin-user-timeout=0' >> /etc/lightdm/lightdm.conf"
 sudo rm /etc/xdg/autostart/piwiz.desktop
-sudo apt-get install git
+sudo apt -y install git
 git clone https://github.com/roocell/scoreboard.git
 
-sudo apt-get install python3-pip
+sudo apt -y install python3-pip
 sudo pip3 install flask
 sudo pip3 install flask-socketio
 sudo pip3 install eventlet
+sudo pip3 uninstall -y python-socketio
+sudo pip3 install python-socketio==5.7.2
+sudo pip3 install pyalsaaudio
+sudo pip3 install pygame
 sudo cp -f ~/scoreboard/scoreboard.service /etc/systemd/system/
 sudo cp -f ~/scoreboard/kiosk.service /lib/systemd/system/
 sudo systemctl daemon-reload
@@ -19,7 +26,7 @@ sudo systemctl enable kiosk.service
 sudo systemctl start kiosk.service
 
 # wifi AP
-sudo apt install hostapd dnsmasq iptables
+sudo apt -y install hostapd dnsmasq iptables
 sudo echo -e "interface wlan0\n  static ip_address=192.168.0.10/24\n  nohook wpa_supplicant" >> /etc/dhcpcd.conf
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo bash -c 'echo -e "interface=wlan0\n  dhcp-range=192.168.0.11,192.168.0.30,255.255.255.0,24h\n" > /etc/dnsmasq.conf'
@@ -34,7 +41,7 @@ sudo bash -c "sed 's/.*net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/' /etc/sysctl
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo iptables-restore < /etc/iptables.ipv4.nat
-sudo apt-get install bridge-utils
+sudo apt -y install bridge-utils
 sudo brctl addbr br0
 sudo brctl addif br0 eth0
 sudo bash -c 'echo -e "auto br0\niface br0 inet manual\nbridge_ports eth0 wlan0"  >> /etc/network/interfaces'
